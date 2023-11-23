fb5f31db25ee10c15917a07b53c6b4115f5045d8
vladnov138 <vladnov138@gmail.com>
2023-11-23 20:45:21 +0800

    Change json format, add visibility settings

--- RandomFilmJSON/app/src/main/java/com/example/randomfilmjson/MainActivity.kt
+++ RandomFilmJSON/app/src/main/java/com/example/randomfilmjson/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.randomfilmjson
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewbinding.ViewBinding
 import com.example.randomfilmjson.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import java.io.InputStreamReader
@@ -14,12 +15,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var movies: List<Movie>
     private var ptr = 0
 
+    private lateinit var staticTvs: Array<TextView>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
         movies = parseMovies(R.raw.movies)
-
+        staticTvs = arrayOf(viewBinding.staticTitleTv, viewBinding.staticDurationTv,
+            viewBinding.staticCountryTv, viewBinding.staticYearTv, viewBinding.staticRatingTv)
+        setStaticTvVisibility(View.INVISIBLE)
         initMovies()
         viewBinding.btnSelect.setOnClickListener(this::showMovie)
         viewBinding.btnRst.setOnClickListener(this::updateMovies)
@@ -27,13 +32,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun showMovie(v: View) {
         if (ptr >= movies.size) {
-            viewBinding.errorTv.text = resources.getText(R.string.err)
+            viewBinding.errorTv.text = getText(R.string.err)
             return
+        }
+        if (viewBinding.staticTitleTv.visibility != View.VISIBLE) {
+            setStaticTvVisibility(View.VISIBLE)
+        }
+        if (viewBinding.errorTv.text != "") {
+            viewBinding.errorTv.text = ""
         }
         val movie = movies[ptr++]
         viewBinding.titleTv.text = movie.title
         viewBinding.durationTv.text = movie.duration
-        viewBinding.countryTv.text = movie.country
+        viewBinding.countryTv.text = movie.country.joinToString(", ")
         viewBinding.yearTv.text = movie.year
         viewBinding.ratingTv.text = movie.rating
     }
@@ -45,12 +56,20 @@ class MainActivity : AppCompatActivity() {
         viewBinding.countryTv.text = ""
         viewBinding.yearTv.text = ""
         viewBinding.ratingTv.text = ""
+        setStaticTvVisibility(View.INVISIBLE)
         initMovies()
     }
 
     private fun initMovies() {
+        viewBinding.errorTv.text = getText(R.string.start_msg)
         movies = movies.shuffled()
         ptr = 0
+    }
+
+    private fun setStaticTvVisibility(visibility: Int = View.INVISIBLE) {
+        for (tv in staticTvs) {
+            tv.visibility = visibility
+        }
     }
 
     private fun parseMovies(id: Int): List<Movie> {
--- RandomFilmJSON/app/src/main/java/com/example/randomfilmjson/Movie.kt
+++ RandomFilmJSON/app/src/main/java/com/example/randomfilmjson/Movie.kt
@@ -1,4 +1,4 @@
 package com.example.randomfilmjson
 
-data class Movie(val title: String, val duration: String, val country: String,
+data class Movie(val title: String, val duration: String, val country: List<String>,
                  val year: String, val rating: String)
