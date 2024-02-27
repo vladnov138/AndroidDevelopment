89ef7db627f994c9481fcccc14d5825e9c2f6fb0
vladnov138 <vladnov138@gmail.com>
2024-02-27 23:37:08 +0800

    init

---
+++ 3.8_practical_work/app/src/main/java/ru/samsung/itschool/myapplication/BlankFragment6.kt
@@ -0,0 +1,37 @@
+package ru.samsung.itschool.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment6.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment6 : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view:View=inflater.inflate(R.layout.fragment_blank, container, false)
+        var btn_back: Button = view.findViewById(R.id.ButtonBack)
+        btn_back.setOnClickListener(View.OnClickListener { view -> view.findNavController().popBackStack()})
+
+        return view
+    }
+}
